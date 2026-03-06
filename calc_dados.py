cd9485799b437c678b97bc491c182bbc063b4635
MARIA CLARA MAGALHÃES DE SOUSA <40690481896@info.local>
2026-03-06 18:43:53 -0300

    feat: distancia euclidiana e produto escalar

--- calc_dados.py
+++ calc_dados.py
@@ -1,3 +1,15 @@
+# Distância Euclidiana e e o Produto Escalar
+def euclidiana(v1, v2):
+    validar_vetores(v1, v2)
+    soma_quadrados = sum((a - b) ** 2 for a, b in zip(v1, v2))
+    return soma_quadrados ** 0.5
+
+
+def produto_escalar(v1, v2):
+    validar_vetores(v1, v2)
+    return sum(a * b for a, b in zip(v1, v2))
+
+
 # Estrutura Inicial e Validação
 def validar_vetores(v1, v2):
     if len(v1) != len(v2):
