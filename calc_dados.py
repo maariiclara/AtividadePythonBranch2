957239f5fc0dcaa6abc225dca55b250ec2643d2a
MARIA CLARA MAGALHÃES DE SOUSA <40690481896@info.local>
2026-03-06 18:53:51 -0300

    feat: metricas de erro mae e mse

--- calc_dados.py
+++ calc_dados.py
@@ -1,3 +1,13 @@
+# Erro
+def mae(v1, v2):
+    validar_vetores(v1, v2)
+    return sum(abs(a - b) for a, b in zip(v1, v2)) / len(v1)
+
+def mse(v1, v2):
+    validar_vetores(v1, v2)
+    return sum((a - b) ** 2 for a, b in zip(v1, v2)) / len(v1)
+
+
 # Estrutura Inicial e Validação
 def validar_vetores(v1, v2):
     if len(v1) != len(v2):
